4d1ea12b2ed6ba39b4ff62a251cb1c77618158f9
wsi1212 <wsi1212@dgsw.hs.kr>
2023-10-11 22:30:37 +0900

    Add: OneMakeOptional
    10/11

--- app/src/main/java/kr/hs/dgsw/dohyunwook/OneMakeOptionActivity.kt
+++ app/src/main/java/kr/hs/dgsw/dohyunwook/OneMakeOptionActivity.kt
@@ -1,13 +1,47 @@
 package kr.hs.dgsw.dohyunwook
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.hs.dgsw.dohyunwook.databinding.ActivityOneMakeOptionBinding
 
 class OneMakeOptionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOneMakeOptionBinding
+    private var isMaleBackgroundReversed = false
+    private var isFemaleBackgroundReversed = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_one_make_option)
+        binding = ActivityOneMakeOptionBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val receivedMessage = intent.getStringExtra("message_key")
 
+        val initialMaleBackground: Drawable = resources.getDrawable(R.drawable.background_unselected_button, null)
+        val reversedMaleBackground: Drawable = resources.getDrawable(R.drawable.background_selected_button, null)
+        val initialFemaleBackground: Drawable = resources.getDrawable(R.drawable.background_unselected_button, null)
+        val reversedFemaleBackground: Drawable = resources.getDrawable(R.drawable.background_selected_button, null)
+        binding.btnMale.setOnClickListener {
+            if ((!isMaleBackgroundReversed && !isFemaleBackgroundReversed) || (!isMaleBackgroundReversed && isFemaleBackgroundReversed)) {
+                if (!isMaleBackgroundReversed) {
+                    isMaleBackgroundReversed = true
+                    binding.btnMale.background = reversedMaleBackground
+                    isFemaleBackgroundReversed = false
+                    binding.btnFemale.background = initialFemaleBackground
+                }
+            }
+        }
+        binding.btnFemale.setOnClickListener {
+            if ((!isMaleBackgroundReversed && !isFemaleBackgroundReversed) || (isMaleBackgroundReversed && !isFemaleBackgroundReversed)) {
+                if (!isFemaleBackgroundReversed) {
+                    isFemaleBackgroundReversed = true
+                    binding.btnFemale.background = reversedFemaleBackground
+                    isMaleBackgroundReversed = false
+                    binding.btnMale.background = initialMaleBackground
+                }
+            }
+        }
+        binding.btnStart.setOnClickListener {
+            
+        }
     }
 }
